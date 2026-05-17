405ff70f5ee4732cee48284596cabedbc5102dac
djvel228 <djvel228@gmail.com>
2026-05-17 13:02:44 +0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,14 @@
+var ofLinks = {
+  "instagram": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "twitter": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "youtube": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "reddit": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "pinterest": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "xhamster": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "pornhub": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "tiktok": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "default": "https://fansly.com/subscriptions/giftcode/OTA1NTMwODA2MjM0MDA1NTA0OjE6MTo5MDU1ZTBiOGEz",
+  "patreon": "http://www.patreon.com/iskraelectrical"
+};
+
+// Auto-generated — do not edit manually
